f2fcee98a710d19857d8230db4e72e824e5c60f0
maklmor <mikiinblack@gmail.com>
2021-06-08 18:38:49 +0200

    amazon aws: collect S3 buckets - closes #8

---
+++ src/cloudinventario_amazon_aws/res_collectors/s3.py
@@ -0,0 +1,77 @@
+import boto3, json
+from pprint import pprint
+
+from cloudinventario.helpers import CloudInvetarioResource
+
+def get_resource_obj(credentials):
+  return CloudInventarioS3(credentials)
+
+class CloudInventarioS3(CloudInvetarioResource):
+
+  def __init__(self, credentials):
+    super().__init__("s3", credentials)
+
+  def _get_client(self):
+    client = boto3.client('s3', aws_access_key_id = self.credentials["access_key"], aws_secret_access_key = self.credentials["secret_key"],
+                                  aws_session_token = self.credentials["session_token"], region_name = self.credentials["region"])
+    return client
+
+  def _fetch(self):
+    data = []
+    paginator = self.client.get_paginator('describe_load_balancers')
+    response_iterator = paginator.paginate()
+
+    marker = None
+    while True:
+      if marker:
+        response_iterator = self.client.describe_load_balancers(Marker=marker)
+      else:
+        response_iterator = self.client.describe_load_balancers()
+
+      for lb in response_iterator['LoadBalancerDescriptions']:
+        data.append((self._process_resource(lb), lb))
+
+      try:
+        marker = response_iterator['Marker']
+      except Exception:
+        break
+
+    return data
+
+  def _process_resource(self, balancer):
+    health_info = self.client.describe_instance_health(LoadBalancerName=balancer['LoadBalancerName'])
+    health_states = {}
+    status = "unknown"
+
+    for instance in health_info['InstanceStates']:
+      state = instance['State']
+      if state == "InService": # if any in service, service is on
+        status = "on"
+      elif status == "unknown" and state == "OutOfService":
+        status = "off"
+      elif status == "unknown" and state == "Unknown":
+        status = "unknown"
+
+      health_states[instance['InstanceId']] = {
+        "state": instance['State']
+      }
+
+    data = {
+      "created": balancer['CreatedTime'],
+      "name": balancer['LoadBalancerName'],
+      "cluster": balancer['AvailabilityZones'],
+      "id": balancer['CanonicalHostedZoneNameID'],
+      "instances": health_states,
+      "public_fqdn": balancer['CanonicalHostedZoneName'],
+      "owner": self.credentials["account_id"],
+      "status": health_states,
+      "is_on": True if status == "on" else False,
+      "scheme": balancer['Scheme'],
+      "subnets": balancer['Subnets']
+    }
+
+    for key, value in data.items():
+      if type(value) in [dict, list]:
+        data[key] = json.dumps(value, default=str)
+
+    return data
