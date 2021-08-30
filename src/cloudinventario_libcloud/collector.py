8c624f842119cdf6c84d1dfdedfc8491a657cc9b
MatejHor <mat.horniak@gmail.com>
2021-08-30 13:26:59 +0200

    Update creating new vm, update _config_keys

--- src/cloudinventario_libcloud/collector.py
+++ src/cloudinventario_libcloud/collector.py
@@ -19,13 +19,10 @@ class CloudCollectorLibcloud(CloudCollector):
 
     def _config_keys():
         return {
-            zone: 'Zone of project',
-            region: 'Region of project',
-            project: 'Project name, only for GCP',
-
+            key: 'First parameters which can be access_key or client_email, Required',
+            secret: 'Second parameters which can be secret_key or private_key, Required',
             driver: 'Driver type for gcp (gce), aws (ec2), Required',
-            first_param: 'First parameters (client_email, access_key), Required',
-            second_param: 'Second parameters (private_key, secret_key), Required',
+            driver_params: 'Additional parameters needed for driver',
         }
 
     def _get_dependencies(self):
@@ -36,17 +33,17 @@ class CloudCollectorLibcloud(CloudCollector):
 
     def _login(self):
         # Get zone or region for cluster field
-        self.zone = self.config['additional_params']['zone'] if 'zone' in self.config['additional_params'] else self.config[
-            'additional_params']['region'] if 'region' in self.config['additional_params'] else None
-        self.project_name = self.config['additional_params'].get('project')
+        self.zone = self.config['driver_params']['zone'] if 'zone' in self.config['driver_params'] else self.config[
+            'driver_params']['region'] if 'region' in self.config['driver_params'] else None
+        self.project_name = self.config['driver_params'].get('project')
         # Load driver to get provider
         ComputeEngine = get_driver(self.config['driver'])
 
         self.driver = ComputeEngine(
-            self.config['first_param'],
-            self.config['second_param'],
+            self.config['key'],
+            self.config['secret'],
             # Pass every additional attribute as dict into computeEngine
-            **self.config['additional_params']
+            **self.config['driver_params']
         )
 
         logging.info("logging config for {} driver type".format(self.config['driver']))
@@ -58,55 +55,48 @@ class CloudCollectorLibcloud(CloudCollector):
 
         for instance in instances:
             # Process instance
-            data.append(self._process_vm(instance))
+            data.append(self._process_vm(instance.__dict__))
         # [self._process_vm(instance) for instance in instances]
 
         logging.info("Collected {} vm".format(len(data)))
         return data
 
     def _process_vm(self, rec):
-        # Attribute to remove from instance
-        to_remove = dict()
-
         # To check if some attribute is object (or array of object) to give every information
-        for key in rec.extra:
-            item = rec.extra[key]
+        for key in rec["extra"]:
+            item = rec["extra"][key]
             # If field is object
             if self._is_not_primitive(item):
                 attributes = dict()
                 for attribute in item.__dict__.items():
                     attributes[attribute[0]] = attribute[1]
-                to_remove[key] = str(attributes)
+                rec["extra"][key] = str(attributes)
             # If field is array of object (need to check first item other will be the same type as first one)
             elif isinstance(item, list) and len(item) > 0 and self._is_not_primitive(item[0]):
                 for object in item:
                     attributes = dict()
                     for attribute in object.__dict__.items():
                         attributes[attribute[0]] = attribute[1]
-                    to_remove[key] = str(attributes)
+                    rec["extra"][key] = str(attributes)
 
-        # Remove key from instance and add object data
-        for key in to_remove:
-            del rec.extra[key]
-            rec.extra[key] = to_remove[key]
-
-        logging.info("new VM name={}".format(rec.name))
+        logging.info("new VM name={}".format(rec["name"]))
         vm_data = {
-            "id": rec.id,
-            "created": rec.created_at,
-            "name": rec.name,
-            "size": rec.size,
-            "image": rec.image,
+            "id": rec["id"],
+            "created": rec["created_at"],
+            "name": rec["name"],
+            "size": rec["size"],
+            "image": rec["image"],
             "cluster": self.zone,
             "project": self.project_name,
-            "primary_ip": rec.public_ips[0] if len(rec.public_ips) > 0 else None,
-            "public_ip": rec.public_ips,
-            "status": rec.state,
-            "is_on": rec.state.lower() == 'running',
-            "tags": rec.extra['labels'] if 'labels' in rec.extra else rec.extra.get('tags'),
-            "extra": rec.extra,
+            "primary_ip": rec["public_ips"][0] if len(rec["public_ips"]) > 0 else None,
+            "public_ip": rec["public_ips"],
+            "private_ip": rec["private_ips"],
+            "status": rec["state"],
+            "is_on": rec["state"].lower() == 'running',
+            "tags": rec["extra"]['labels'] if 'labels' in rec["extra"] else rec["extra"].get('tags'),
         }
-        return self.new_record('vm', vm_data, vm_data)
+
+        return self.new_record('vm', vm_data, rec)
 
     def _logout(self):
         self.driver = None
